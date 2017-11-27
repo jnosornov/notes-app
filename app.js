12cd4ce4323466e2b55ca2f922423318b9dc8174
ColdWater <you@example.com>
2017-11-27 13:21:56 -0500

    app for add, list, read, or remove a note first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+console.log("Starting app.js");
+
+const fs = require('fs');
+const _ = require('lodash');
+const yargs = require('yargs');
+
+const notes = require('./notes.js');
+
+const argv = yargs.argv;
+var command = argv._[0];
+
+console.log('Process', process.argv);
+console.log('Yargs', argv);
+
+if(command === 'add') {
+    notes.addNote(argv.title, argv.body);
+} else if(command === 'list') {
+    notes.getAll();
+} else if(command === 'read') {
+    notes.readNote(argv.title);
+} else if(command === 'remove') {
+    notes.removeNote(argv.title);
+} else {
+    console.log('Command not recognized');
+}
---
+++ notes.js
@@ -0,0 +1,44 @@
+console.log('Startin notes.js');
+
+const fs = require('fs');
+
+var addNote = (title, body) => {
+    var notes = [];
+    var note = {
+        title,
+        body
+    };
+
+    try {
+        var notesString = fs.readFileSync('nodes-data.json');
+        notes = JSON.parse(notesString);
+    } catch(e) {
+
+    }
+
+    var duplicateNotes = notes.filter((note) => note.title === title);
+
+    if(duplicateNotes.length === 0) {
+        notes.push(note);
+        fs.writeFileSync('nodes-data.json', JSON.stringify(notes));        
+    }
+};
+
+var getAll = () => {
+    console.log('Getting all notes');
+};
+
+var readNote = (title) => {
+    console.log('Reading note', title);
+};
+
+var removeNote = (title) => {
+    console.log('Removing note', title);
+};
+
+module.exports =  {
+    addNote,
+    getAll,
+    readNote,
+    removeNote
+};
