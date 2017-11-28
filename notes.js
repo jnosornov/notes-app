b006b44f587656179d00a8f693b3e28bf3e4e741
Johnathan <you@example.com>
2017-11-28 07:43:21 -0500

    command line list functionality and clean up the code

--- notes.js
+++ notes.js
@@ -1,5 +1,3 @@
-console.log('Startin notes.js');
-
 const fs = require('fs');
 
 var fetchNotes = () => {
@@ -32,7 +30,7 @@ var addNote = (title, body) => {
 };
 
 var getAll = () => {
-    console.log('Getting all notes');
+    return fetchNotes();
 };
 
 var getNote = (title) => {
